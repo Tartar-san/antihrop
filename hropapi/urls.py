bbfe90a693e6e28d4e8ab73f274d6f296933d8d5
aodarc <odarchenko@ex.ua>
2016-08-20 19:53:26 +0300

    added Hrops list

--- hropapi/urls.py
+++ hropapi/urls.py
@@ -8,12 +8,12 @@ from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
-router.register(r'hrops', HropViewSet)
 
 urlpatterns = [
+    url(r'^token-auth/', views.obtain_auth_token),
     url(r'^users/register', RegistrationsView.as_view()),
     url(r'^', include(router.urls)),
-    url(r'^token-auth/', views.obtain_auth_token)
+    url(r'^hrops', HropsView.as_view())
     # url(r'^profiles/(?P<user_id>[0-9]+)$', ),
 
 ]
